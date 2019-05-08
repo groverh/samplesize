a71f1513aa4319eed9be2bfe06dada63dc0f279c
groverh <gdhwang@g.ntu.edu.tw>
2019-05-08 16:34:58 +0800

    Added sample frequency files Added sample code

---
+++ s1.r
@@ -0,0 +1,12 @@
+#read in frequency data
+af <- read.table("./a.txt")
+bf <- read.table("./b.txt")
+cf <- read.table("./c.txt")
+drf <- read.table("./dr.txt")
+
+#af=af/100.
+#bf=bf/100.
+#cf=cf/100.
+#drf=drf/100.
+
+
